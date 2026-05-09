ac07db0c75268e0c33078a5f01715eac95dd22d6
Pawanchathumal <E131110@esoft.academy>
2026-05-09 11:02:04 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export', // Enables static HTML export
+  images: { unoptimized: true }, // Required for static sites
 };
-
+module.exports = nextConfig;
 export default nextConfig;
